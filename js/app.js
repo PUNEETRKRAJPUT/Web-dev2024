24e14ed75fa58432ef73e0bf363a700c7af7acf8
PUNEETRKRAJPUT <128739405+PUNEETRKRAJPUT@users.noreply.github.com>
2024-01-29 20:51:27 +0530

    29-01-2024

---
+++ js/app.js
@@ -0,0 +1,39 @@
+let guess=prompt("guess what tange walla wants ");
+
+
+while(true){
+   
+    if(guess == "kuch nahi"){
+        prompt("thoda try krta toh pta chl jaata !!");
+        break;
+    }
+    if(guess=="tum"){
+        prompt("yeah you won !!!!!!!!!!!");
+        break;
+    }else if(guess == "ganna"){
+        guess=prompt("lammba h madam lamba teri jaan kasam lamba");
+    
+    }else if(guess == "kela"){
+        guess=prompt("mota h madam mota teri jaan kasam mota");
+        
+
+    }else if(guess == "rasgulla"){
+        guess=prompt("chipke h madam chipke teri jaan kasam chipke");
+        
+    }else if(guess =="nimboo"){
+        guess=prompt("khatta h madam khatta teri jaan kasam khatta");
+        
+    } else if(guess =="papita"){
+        guess=prompt("mota h madam mota teri jaan kasam mota");
+        
+    }else if(guess =="jaman"){
+        guess=prompt(" kaali  h madam kaali teri jaan kasam kaaali");
+       
+    }else if(guess =="baigan"){
+        guess=prompt("chikne h madam chikne teri jaan kasam chikne");
+        
+    }else {
+        guess=prompt("guess what tange walla wants ");
+    };
+
+};
